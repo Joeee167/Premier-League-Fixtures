83ed11909a52f67fd07f485bc2918f077070462d
Joeee167 <youssefgamer037@gmail.com>
2024-09-03 23:13:03 +0300

    Added new html file and updated script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-ff4b2b
-ff416c
+function loadHeader()
+{
+    // Fetch the header.html file to use as header of all pages
+    fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector(".head-div").innerHTML = data;
+    })
+
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+loadHeader();
